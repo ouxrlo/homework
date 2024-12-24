8f6c416ced4c2df6fc1fcc6206999553b8eb0f47
“personal” <ouxrlo3@gmail.com>
2024-12-24 20:35:17 +0900

    df_check

--- hw1/hw.py
+++ hw1/hw.py
@@ -13,3 +13,13 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# 데이터 로드
+file_path = '/Users/t2023-m0093/Desktop/homework/hw1/housingdata.csv'
+housing_data = pd.read_csv(file_path)
+
+# 데이터 확인
+print(housing_data.head())
+print(housing_data.info())
+print(housing_data.describe())
+
