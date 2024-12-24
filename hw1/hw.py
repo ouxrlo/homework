293abf6e31fb28d9bec10b8496da8ea3f5773832
“personal” <ouxrlo3@gmail.com>
2024-12-24 22:45:31 +0900

    df_14

--- hw1/hw.py
+++ hw1/hw.py
@@ -1,10 +1,3 @@
-
-# 가상환경 설정
-# conda create --name myenv38 python=3.8
-# conda activate myenv38
-# conda install pandas numpy xgboost scikit-learn matplotlib seaborn
-
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -71,7 +64,6 @@ plt.xlabel('Actual Prices')
 plt.ylabel('Predicted Prices')
 plt.title('Actual vs Predicted Prices')
 plt.show()
-
 
 
 
